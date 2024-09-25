8701129a044f94bf0d6d7555e8032b420b6355e1
Fouad-berry <fidriss96@gmail.com>
2024-09-25 18:37:08 +0200

    update dashboard style

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,8 +5,8 @@ export default function DashboardLayout({
   }>) {
     return (
         <div className="h-screen flex">
-            <div className="w-[14%] md:w[8%] lg:w-[16%] xl:w-[14%] bg-pink-200">Left</div>
-            <div className="w-[86%] md:[92%] lg:w-[84%] xl:w-[86%] bg-gray-200">Right</div>
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-pink-200">Left</div>
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-gray-200">Right</div>
             {children}
         </div>
     );

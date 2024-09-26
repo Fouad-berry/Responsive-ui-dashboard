e2327007c3de039261b1585f2f25178c9609c347
Fouad-berry <fidriss96@gmail.com>
2024-09-26 20:34:24 +0200

    update admin dashboard menu item

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import Menu from "@/components/Menu";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,10 +10,11 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
             <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-pink-200">
-                <Link href="/" className="flex items-center justify-center gap-2">
-                    <Image src="/logo.png" alt="logo" width={32} height={32}/>
+                <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+                    <Image src="/logo.png" alt="logo" width={40} height={40}/>
                     <span className="hidden lg:block">AtypikHouse</span>
                 </Link>
+                <Menu/>
             </div>
             <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-gray-200">R</div>
             {children}

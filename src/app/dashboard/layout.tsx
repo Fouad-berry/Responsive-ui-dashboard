044e12fc14f1e06dd91592351ede25b8b7362fe0
Fouad-berry <fidriss96@gmail.com>
2024-09-27 15:10:34 +0200

    update layout style

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,14 +9,14 @@ export default function DashboardLayout({
   }>) {
     return (
         <div className="h-screen flex">
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-pink-200">
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
                 <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
                     <Image src="/logo.png" alt="logo" width={40} height={40}/>
                     <span className="hidden lg:block">AtypikHouse</span>
                 </Link>
                 <Menu/>
             </div>
-            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-gray-200">R</div>
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">R</div>
             {children}
         </div>
     );

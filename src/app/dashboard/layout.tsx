3968b85081f03f3bcb33f27f4c71a2c15370323a
Fouad-berry <fidriss96@gmail.com>
2024-09-27 15:11:50 +0200

    update layout style v2

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
                 </Link>
                 <Menu/>
             </div>
-            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">R</div>
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]"></div>
             {children}
         </div>
     );

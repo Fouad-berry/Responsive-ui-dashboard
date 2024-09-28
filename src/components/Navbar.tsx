05d90bcde2329c12d01155cf1716c3a09926273b
Fouad-berry <fidriss96@gmail.com>
2024-09-28 10:54:18 +0200

    update menu style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,15 @@ const Navbar = () => {
         <div className='flex items-center justify-between p-4'>
             <div className='hidden md:flex'>
                 <Image src="/search.png" alt='' width={14} height={14} />
-                <input type="text"  placeholder='Search'/>
+                <input type="text"  placeholder='Search...'/>
             </div>
-            <div className=''>
-
+            <div className='flex items-center gap-6'>
+                <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+                    <Image src="/message.png" alt='messages' width={20} height={20} />
+                </div>
+                <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
+                    <Image src="/announcement.png" alt='messages' width={20} height={20} />
+                </div>
             </div>
         </div>
     );

56c16d330101b21cce67b3e5a19f5d63d096d2d0
Fouad-berry <fidriss96@gmail.com>
2024-09-30 21:24:24 +0200

    update UserCard style v2

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserCard = ({type}:{type:string}) => {
     return (
-        <div className='rounded-2xl odd:bg-[#d7e913]'>
+        <div className='rounded-2xl odd:bg-[#d7e913] even:bg-[#4d4e45] p-4'>
             UserCard
         </div>
     );

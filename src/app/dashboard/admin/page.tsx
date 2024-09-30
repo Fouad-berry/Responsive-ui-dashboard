662878a3ebc2c7bfd2f6a8d51a36b3fceda6d011
Fouad-berry <fidriss96@gmail.com>
2024-09-30 21:12:36 +0200

    add style to usercard

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,9 +1,17 @@
+import UserCard from '@/components/UserCard';
 import React from 'react';
 
 const AdminPage = () => {
     return (
         <div className='p-4 flex gap-4 flex-col md:flex-row'>
-            <div className='w-full lg:w-2/3'>L</div>
+            <div className='w-full lg:w-2/3'>
+                <div className=''>
+                    <UserCard type='student' />
+                    <UserCard type='teacher' />
+                    <UserCard type='parent' />
+                    <UserCard type='staff' />
+                </div>
+            </div>
             <div className='w-full lg:w-1/3'>R</div>
         </div>
     );

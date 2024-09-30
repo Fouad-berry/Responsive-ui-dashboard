2b074a16764953015ba07a2ac284b5d54a110844
Fouad-berry <fidriss96@gmail.com>
2024-09-30 21:19:16 +0200

    update UserCard style

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -5,7 +5,7 @@ const AdminPage = () => {
     return (
         <div className='p-4 flex gap-4 flex-col md:flex-row'>
             <div className='w-full lg:w-2/3'>
-                <div className=''>
+                <div className='flex gap-4 justify-between'>
                     <UserCard type='student' />
                     <UserCard type='teacher' />
                     <UserCard type='parent' />
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserCard = ({type}:{type:string}) => {
     return (
-        <div className=''>
+        <div className='rounded-2xl odd:bg-[#d7e913]'>
             UserCard
         </div>
     );

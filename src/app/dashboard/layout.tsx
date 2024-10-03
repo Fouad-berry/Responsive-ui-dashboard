e036eb278cc2e20c8ec9d321a8d240db2d69fb8f
Fouad-berry <fidriss96@gmail.com>
2024-10-03 20:11:53 +0200

    ad style

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
   }>) {
     return (
         <div className="h-screen flex">
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-6">
                 <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
                     <Image src="/logo.png" alt="logo" width={40} height={40}/>
                     <span className="hidden lg:block">AtypikHouse</span>

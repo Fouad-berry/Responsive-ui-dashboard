610b30a8c03c1584e6bd946a6fb2cbc43e773703
Fouad-berry <fidriss96@gmail.com>
2024-10-06 11:19:43 +0200

    update layout.tsx for dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({
             <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-6">
                 <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
                     <Image src="/logo.png" alt="logo" width={40} height={40}/>
-                    <span className="hidden lg:block">Welcome to AtypikHouse</span>
+                    <span className="hidden lg:block">Welcome to AtypikHouse Admin Panel</span>
                 </Link>
                 <Menu/>
             </div>

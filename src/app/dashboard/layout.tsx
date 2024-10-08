d5189bd9613e1d4df091ed0a05a984a18d5b4b23
Fouad-berry <fidriss96@gmail.com>
2024-10-08 19:35:32 +0200

    update layout dashboard page v3

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -19,7 +19,7 @@ export default function DashboardLayout({
             </div>
             <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
                 <Navbar/>
-                The site is on development , please come later
+                The site is on development , please come later (...)
                 {children}
             </div>
         </div>
